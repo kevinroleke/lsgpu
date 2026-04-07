f7ff8cb52de79f934954605d7896085fefa4ce8f
kevinroleke <kevinroleke@gmail.com>
2026-04-07 19:01:22 -0400

    rxknephew

---
+++ entities/rxknephew.py
@@ -0,0 +1,25 @@
+from ansi import CYAN
+from .base import EntitySpec
+
+SPEC = EntitySpec("rxknephew", color=CYAN, frames=[
+    [
+        " )|( )|( )|(  .------------.",
+        " (  @   @  )  | SPIN BOUT  |",
+        "  \\  ___  /   |    U! FR   |",
+        "   /-----\\    '-----.------'",
+        "  /| RXK |\\        /        ",
+        " / |     | \\               ",
+        "   | /\\  |                 ",
+        "   |/  \\ |                 ",
+    ],
+    [
+        ")|( )|( )|(   .------------.",
+        " (  @   @  )  | IM WEALTHY |",
+        "  \\  ___  /   | GET THAT   |",
+        "   /-----\\    |  MONEY FR  |",
+        "  /| RXK |\\   '-----.------'",
+        " / |     | \\        /      ",
+        "   |  /\\ |                 ",
+        "   | /  \\|                 ",
+    ],
+])
